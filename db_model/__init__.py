bdbec9cfba4c31105c9520a1b70bd1f8c9850108
FunnyAWM <1142978300@qq.com>
2026-01-31 20:56:58 +0800

    更新了db_model包信息

--- db_model/__init__.py
+++ db_model/__init__.py
@@ -5,6 +5,13 @@ from .cp import Cp
 from .questionnaire import Questionnaire
 from .music import Music
 
+"""
+PostgreSQL数据库模型定义
+
+定义了用于投票数据保存的数据模型。
+"""
+__version__ = "1.0.0"
+__author__ = "FunnyAWM"
 __all__ = [
     "User",
     "Character",
