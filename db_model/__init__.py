5de47a4b63f2a2a66084e76d08bf977dd3b07ac7
FunnyAWM <1142978300@qq.com>
2026-01-31 20:45:57 +0800

    修复Pylint中导入错误的问题

--- db_model/__init__.py
+++ db_model/__init__.py
@@ -1,5 +1,15 @@
+from .character import Character
+from .base import Base
 from .user import User
+from .cp import Cp
+from .questionnaire import Questionnaire
+from .music import Music
 
 __all__ = [
-    "User"
+    "User",
+    "Character",
+    "Base",
+    "Cp",
+    "Questionnaire",
+    "Music"
 ]
