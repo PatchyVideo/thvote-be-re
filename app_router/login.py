1580c893b68e1b5e4839335a5d938342140fabc9
FunnyAWM <1142978300@qq.com>
2026-02-10 16:00:27 +0800

    创建了部分用户功能（待实现）

---
+++ app_router/login.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from dao import LoginData, RegisterData
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+@router.post("/login")
+def login(user_data: LoginData):
+    pass
+
+@router.post("/register")
+def register(register_data: RegisterData):
+    pass
